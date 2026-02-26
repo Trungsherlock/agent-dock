b2e0c3592d268156de4ecce18d79df9892343ac8
Trung <trungnguyenquochn2202@gmail.com>
2026-02-25 23:44:42 -0500

    wire everything up in extension.ts

--- src/extension.ts
+++ src/extension.ts
@@ -1,25 +1,76 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { SessionManager } from './managers/sessionManager';
+import { SessionTreeProvider } from './views/sessionTreeProvider';
+import { SessionTreeItem } from './views/sessionTreeItem';
+import { CATEGORIES } from './constants/categories';
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+	const sessionManager = new SessionManager();
+	const treeProvider = new SessionTreeProvider(sessionManager);
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "agentdock" is now active!');
+	vscode.window.registerTreeDataProvider('agentdock.sessionsView', treeProvider);
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('agentdock.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from agentdock!');
-	});
+	vscode.window.onDidCloseTerminal(terminal => {
+		sessionManager.removeByTerminal(terminal);
+	}, null, context.subscriptions);
 
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(
+		vscode.commands.registerCommand('agentdock.newSession', async () => {
+			const name = await vscode.window.showInputBox({
+				prompt: 'Session name',
+				placeHolder: 'e.g. Fix login bug',
+			});
+			if (!name) {
+				return;
+			}
+
+			const picked = await vscode.window.showQuickPick(
+				CATEGORIES.map(c => ({ label: `$(${c.icon}) ${c.label}`, id: c.id })),
+				{ placeHolder: 'Select a category' }
+			);
+			if (!picked) {
+				return;
+			}
+
+			const terminal = vscode.window.createTerminal({
+				name,
+				iconPath: new vscode.ThemeIcon(
+					CATEGORIES.find(c => c.id === picked.id)!.icon
+				),
+			});
+			terminal.show();
+
+			sessionManager.add(name, picked.id, terminal);
+		})
+	);
+
+	context.subscriptions.push(
+        vscode.commands.registerCommand('agentdock.focusSession', (item: SessionTreeItem) => {
+            item.session.terminal.show();
+        })
+    );
+	
+	context.subscriptions.push(
+		vscode.commands.registerCommand('agentdock.endSession', (item: SessionTreeItem) => {
+			item.session.terminal.dispose();
+			sessionManager.remove(item.session.id);
+		})
+	);
+
+	context.subscriptions.push(
+		vscode.commands.registerCommand('agentdock.renameSession', async (item: SessionTreeItem) => {
+			const newName = await vscode.window.showInputBox({
+				prompt: 'New session name',
+				value: item.session.name,
+			});
+			if (!newName) { return;}
+			sessionManager.rename(item.session.id, newName);
+		})
+	);
 }
 
 // This method is called when your extension is deactivated
--- src/views/sessionTreeProvider.ts
+++ src/views/sessionTreeProvider.ts
@@ -3,6 +3,21 @@ import { SessionManager } from '../managers/sessionManager';
 import { SessionTreeItem } from './sessionTreeItem';
 import { CATEGORIES } from '../constants/categories';
 
-// export class SessionTreeProvider implements vscode.TreeDataProvider<SessionTreeItem> {
+export class SessionTreeProvider implements vscode.TreeDataProvider<SessionTreeItem> {
+    private _onDidChangeTreeData = new vscode.EventEmitter<void>();
+    readonly onDidChangeTreeData = this._onDidChangeTreeData.event;
 
-// }
+    constructor(private sessionManager: SessionManager) {
+        sessionManager.onDidChange(() => {
+            this._onDidChangeTreeData.fire();
+        });
+    }
+
+    getTreeItem(element: SessionTreeItem): vscode.TreeItem {
+        return element;
+    }
+
+    getChildren(): SessionTreeItem[] {
+        return this.sessionManager.getAll().map(s => new SessionTreeItem(s));
+    }
+}
